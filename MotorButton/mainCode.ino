f20cd1c0af5e84b155cff6c8dadaecf7964e4b47
Abdellatif <c2.daliabdellatif@gmail.com>
2024-05-23 15:57:49 +0100

    Add files via upload

---
+++ MotorButton/mainCode.ino
@@ -0,0 +1,24 @@
+volatile bool state = false;
+
+void function(void)
+{
+  if(state == false)
+    state = true;
+  else
+    state = false;
+}
+
+void setup() 
+{
+  pinMode(2, INPUT);
+  pinMode(4, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(2), function, RISING);
+}
+
+void loop() 
+{
+  if(state == true)
+    digitalWrite(4, HIGH);
+  else
+    digitalWrite(4, LOW);
+}
